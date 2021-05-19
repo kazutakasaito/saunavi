6c473338ad55e6df1dfc082cb089da6f72047173
kazutaka.saito <kazutaka.saito@g.softbank.co.jp>
2021-05-19 09:05:16 +0900

    Navbar追加

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import BootstapVue, { NavbarPlugin } from 'bootstrap-vue';
+import BootstrapVue,
+{
+  IconsPlugin,
+  NavbarPlugin
+} from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
-Vue.use(BootstapVue);
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 Vue.use(NavbarPlugin);
 
 new Vue({
